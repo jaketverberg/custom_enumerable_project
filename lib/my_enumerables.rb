0af941c744a185b3ce29dd57e63cda11aec8f0ef
jaketverberg <54115094+jaketverberg@users.noreply.github.com>
2022-02-14 11:15:33 -0600

    Updates several my_functions

--- lib/my_enumerables.rb
+++ lib/my_enumerables.rb
@@ -1,12 +1,45 @@
+# frozen_string_literal: true
+
 module Enumerable
   # Your code goes here
   def my_each_with_index
-    for i in 0..self.size-1
-      yield self[i], i
-    end
+    return self.to_enum unless block_given?
+
+    self.size.times { |i| yield self[i], i }
+    self
   end
 
+  def my_all?(&block)
+    self.my_each { |i| return false unless block.call(i) }
+    true
+  end
 
+  def my_any?(&block)
+    self.my_each { |i| return true if block.call(i) }
+    false
+  end
+
+  def my_count(&block)
+    return self.size unless block_given?
+    counter = 0
+    self.my_each { |i| counter += 1 if block.call(i) }
+    counter
+  end
+
+  def my_inject(accumulator, &block)
+    unless accumulator
+      accumulator = self.first
+      for i in 0..self.size-2
+        accumulator = block.call(accumulator, self[i + 1])
+      end
+    end
+    if accumulator
+      for i in 0..self.size-1
+        accumulator = block.call(accumulator, self[i])
+      end
+    end
+    accumulator
+  end
 end
 
 # You will first have to define my_each
@@ -16,9 +49,12 @@ end
 class Array
   # Define my_each here
   def my_each
-
     for k, v in self do
       yield k, v
     end
   end
 end
+
+arr = [1, 1, 2, 3, 5, 8, 13, 21, 34]
+
+arr.my_each_with_index
